f0d80f9a818d8a8aaa2670df8cf4a314ac9c0500
bobbykingsada <kingsadabobby@gmail.com>
2025-08-21 10:19:24 -0700

    adding supabase configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <ColorSchemeScript />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <MantineProvider theme={theme}>
           <Notifications />
           {children}
